b7391ba3145b2b499e2ec764e951b507121f6097
Haider Hassoun <e.h77edar@gmail.com>
2026-09-08 14:54:28 +0300

    Force IPv4 for Gmail SMTP

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,9 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const User = require('../models/User');
 
